181d6a1e6c70cf4545f0dfe22704e61ed061d8a1
Felix Boström <felix@bostrom.se>
2025-03-11 22:33:01 +0100

    added cisco support

---
+++ integrations/cisco_snmp.py
@@ -0,0 +1,95 @@
+import asyncio
+from pysnmp.hlapi.asyncio import *
+from pysnmp.hlapi.v3arch.asyncio import UdpTransportTarget
+import re
+
+# convert mac address to valid format for comparison in arp-table.json
+def format_mac(mac):
+    if not mac:
+        return mac
+    else:
+        # remove '0x' prefix if present
+        if mac.startswith("0x"):
+            mac = mac[2:]
+
+        # split non special char mac and add colon
+        if len(mac) == 12:
+            mac = [mac[i:i+2] for i in range(0, len(mac), 2)]
+            mac = ":".join(mac).lower()
+
+        return mac
+
+# extract info from snmp
+def extract_info(oid, value):
+
+    vlan_match = re.search(r"\.2\.(\d+)\.1\.", oid)
+    vlan = vlan_match.group(1) if vlan_match else None
+
+    ipv4_octets = oid.split(".")[-4:]
+    ipv4 = ".".join(ipv4_octets) if len(ipv4_octets) == 4 else None
+
+    mac = value.strip() if value else None
+
+    return vlan, ipv4, mac
+
+# convert oid string to tuple for comparison
+def oid_to_tuple(oid):
+    return tuple(map(int, oid.strip('.').split('.')))
+
+# check if oid is within the specified range
+def is_oid_in_range(current_oid, start_oid, stop_oid):
+    current_oid_tuple = oid_to_tuple(current_oid)
+    start_oid_tuple = oid_to_tuple(start_oid)
+    stop_oid_tuple = oid_to_tuple(stop_oid)
+
+    # esnure current oid starts with start oid prefix
+    if not current_oid_tuple[:len(start_oid_tuple)] == start_oid_tuple:
+        return False
+
+    # check so current oid is less or equal to stop oid
+    return current_oid_tuple <= stop_oid_tuple
+
+# snmp walk between start and stop OID
+async def snmp_walk(host, port, community, start_oid, stop_oid):
+    snmp_engine = SnmpEngine()
+    snmp_community = CommunityData(community)
+    snmp_transport = await UdpTransportTarget.create((host, port))
+    snmp_context = ContextData()
+    next_oid = start_oid
+    results = []
+
+    while True:
+        errorIndication, errorStatus, errorIndex, varBinds = await next_cmd(
+            snmp_engine,
+            snmp_community,
+            snmp_transport,
+            snmp_context,
+            ObjectType(ObjectIdentity(next_oid))
+        )
+
+        if errorIndication:
+            print(f"Error: {errorIndication}")
+            break
+        elif errorStatus:
+            print(f"Error: {errorStatus.prettyPrint()}")
+            break
+        else:
+            for varBind in varBinds:
+                oid, value = varBind
+                current_oid = str(oid)
+
+                if not is_oid_in_range(current_oid, start_oid, stop_oid):
+                    return results  # Return the collected results
+
+                #results.append(f"{oid.prettyPrint()} = {value.prettyPrint()}")
+                vlan,ipv4,mac = extract_info(current_oid, value.prettyPrint())
+                results.append({"vlan": vlan, "ip": ipv4, "mac": format_mac(mac)})
+
+                next_oid = oid
+
+    return results
+
+# function called from main netmon-lite
+def get(host, port, community, start_oid, stop_oid):
+    results = asyncio.run(snmp_walk(host, port, community, start_oid, stop_oid))
+    return results
--- netmon_lite.py
+++ netmon_lite.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import yaml
 import requests
 from tabulate import tabulate
-from send_mail import send_mail
+from integrations import alerts,cisco_snmp,opnsense_api
 
 # current date and time for logging findings
 time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -20,21 +20,30 @@ with open("arp-table.json", "r") as f:
 history = [d['MACAddr'] for d in data['history']]
 
 for arp in config["arp"]:
+    # OPNSense HTTP API Calls
     if arp == "opnsense":
-        # calls opnsense api for arp table
-        ipv4 = config["arp"][arp]["ip"]
-        url = "https://{0}/api/diagnostics/interface/getArp".format(ipv4)
-        r = requests.get(url, verify = False, auth = (config["arp"][arp]["api"], config["arp"][arp]["secret"]), timeout=10)
-        response = json.loads(r.text)
-        
-        # reading through response for unknown mac addresses
+        response = opnsense_api.get(
+            config["arp"][arp]["ip"],
+            config["arp"][arp]["api"],
+            config["arp"][arp]["secret"]
+        )
         for line in response:
             if line["mac"] not in history:
-                # adding new devices to json
-                new_item = {"Time" : time, "MACAddr" : line["mac"], "IPv4" : line['ip'], "Int" : line['intf'], "Manu" : line['manufacturer']}
+                new_item = {"Time" : time, "MACAddr" : line["mac"].lower(), "IPv4" : line['ip'], "Int" : line['intf'], "Manu" : line['manufacturer']}
                 new_devices.append(new_item)
-    if arp == "cisco":
-        # coming soon
+    # Cisco IOS SNMP
+    elif arp == "cisco":
+        response = cisco_snmp.get(
+            config["arp"][arp]['ip'],
+            config["arp"][arp]['port'],
+            config["arp"][arp]['snmp'],
+            config["arp"][arp]['oid'],
+            config["arp"][arp]['stop_oid']
+        )
+        for line in response:
+            if line['mac'] not in history:
+                new_item = {"Time" : time, "MACAddr" : line["mac"].lower(), "IPv4" : line['ip'], "Int" : line['vlan'], "Manu" : ""}
+                new_devices.append(new_item)
 
 if new_devices != []:
     print("Found new network devices! ({0})".format(len(new_devices)))
@@ -42,13 +51,13 @@ if new_devices != []:
     # alerting using all configured channels
     for alert in config["alert"]:
         if alert == "gmail":
-            send_mail(
+            alerts.send_mail(
                 config["alert"][alert]["smtp"],config["alert"][alert]["port"],
                 config["alert"][alert]["from"]["usr"],config["alert"][alert]["from"]["pwd"],
                 config["alert"][alert]["to"],
                 "WARNING: New network device found!",new_devices
             )
-    
+
     # exporting to history file for next run
     data["history"].extend(new_devices)
     with open("arp-table.json", "w") as f:
